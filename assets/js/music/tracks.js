772e89811cfdc87591597b58f385aeeee864a862
IIsaev <ii@intechnology.ru>
2018-10-30 17:24:36 +0300

    Create cups

--- assets/js/music/tracks.js
+++ assets/js/music/tracks.js
@@ -10,64 +10,64 @@ const Albums = [
                 name: 'В Снах Героев',
                 en_name: 'In Sleep of Heroes',
                 file: 'assets/musics/in_sleep_of_heroes.mp3',
-                tabs: 'assets/musics/in_sleep_of_heroes.gpx',
-                text: 'assets/musics/in_sleep_of_heroes.txt'
+                tabs: 'assets/musics/tabs/in_sleep_of_heroes.gpx',
+                text: 'assets/musics/texts/in_sleep_of_heroes.txt'
             },
             {
                 name: 'Маска',
                 en_name: 'The Mask',
                 file: 'assets/musics/mask.mp3',
-                tabs: 'assets/musics/mask.gpx',
-                text: 'assets/musics/mask.txt'
+                tabs: 'assets/musics/tabs/mask.gpx',
+                text: 'assets/musics/texts/mask.txt'
             },
             {
                 name: 'Танец',
                 en_name: 'The Dance',
                 file: 'assets/musics/dance.mp3',
-                tabs: 'assets/musics/dance.gpx',
-                text: 'assets/musics/dance.txt'
+                tabs: 'assets/musics/tabs/dance.gpx',
+                text: 'assets/musics/texts/dance.txt'
             },
             {
                 name: 'Сварожий круг',
                 en_name: 'Svarog circle',
                 file: 'assets/musics/svarog_circle.mp3',
-                tabs: 'assets/musics/svarog_circle.gpx',
-                text: 'assets/musics/svarog_circle.txt'
+                tabs: 'assets/musics/tabs/svarog_circle.gpx',
+                text: 'assets/musics/texts/svarog_circle.txt'
             },
             {
                 name: 'Бездомный пёс',
                 en_name: 'Homeless dog',
                 file: 'assets/musics/homeless_dog.mp3',
-                tabs: 'assets/musics/homeless_dog.gpx',
-                text: 'assets/musics/homeless_dog.txt'
+                tabs: 'assets/musics/tabs/homeless_dog.gpx',
+                text: 'assets/musics/texts/homeless_dog.txt'
             },
             {
                 name: 'Испытание огнём',
                 en_name: 'Fire Test',
                 file: 'assets/musics/fire_test.mp3',
-                tabs: 'assets/musics/fire_test.gpx',
-                text: 'assets/musics/fire_test.txt'
+                tabs: 'assets/musics/tabs/fire_test.gpx',
+                text: 'assets/musics/texts/fire_test.txt'
             },
             {
                 name: 'Нас ждёт огонь',
                 en_name: 'We are waiting for the fire',
                 file: 'assets/musics/we_are_waiting_for_the_fire.mp3',
-                tabs: 'assets/musics/we_are_waiting_for_the_fire.gpx',
-                text: 'assets/musics/we_are_waiting_for_the_fire.txt'
+                tabs: 'assets/musics/tabs/we_are_waiting_for_the_fire.gpx',
+                text: 'assets/musics/texts/we_are_waiting_for_the_fire.txt'
             },
             {
                 name: 'Прошлое - прах',
                 en_name: 'The past is dust',
                 file: 'assets/musics/the_past_is_dust.mp3',
-                tabs: 'assets/musics/the_past_is_dust.gpx',
-                text: 'assets/musics/the_past_is_dust.txt'
+                tabs: 'assets/musics/tabs/the_past_is_dust.gpx',
+                text: 'assets/musics/texts/the_past_is_dust.txt'
             },
             {
                 name: 'Ворон',
                 en_name: 'The Raven',
                 file: 'assets/musics/raven.mp3',
-                tabs: 'assets/musics/raven.gpx',
-                text: 'assets/musics/raven.txt'
+                tabs: 'assets/musics/tabs/raven.gpx',
+                text: 'assets/musics/texts/raven.txt'
             }
         ]
     }
